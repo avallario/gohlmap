9f17d3681da8ff766bc50c9bf97f72f0269aeb4d
Anthony <anthony.vallario@hotmail.com>
2017-09-05 03:11:51 -0400

    hlmazegen now automatically calls build tools

---
+++ cmd/hlmazegen/README.txt
@@ -0,0 +1,10 @@
+To use hlmazegen:
+
+Create a directory containing the following:
+hlmazegen.exe
+cell.map, empty.map
+hlcsg.exe, hlbsp.exe, and hlvis.exe (these can be acquired from http://zhlt.info/download-zhlt.html)
+
+Edit empty.map: in the empty quotes next to "wad", enter the path to a copy of halflife.wad
+
+Run hlmazegen.exe
--- cmd/hlmazegen/mazegen.go
+++ cmd/hlmazegen/mazegen.go
@@ -4,11 +4,30 @@ import (
 	"github.com/avallario/gohlmap/cmd/hlmazegen/generator"
 	"github.com/avallario/gohlmap/mapio"
 	"math/rand"
+	"os/exec"
 	"time"
 )
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	hlmap := generator.GenerateMap(generator.GenerateMaze(20, 20))
 	mapio.ExportMap(hlmap, "output.map")
+
+	csg_cmd := exec.Command("hlcsg", "output.map")
+	csg_err := csg_cmd.Run()
+	check(csg_err)
+
+	bsp_cmd := exec.Command("hlbsp", "output.map")
+	bsp_err := bsp_cmd.Run()
+	check(bsp_err)
+
+	vis_cmd := exec.Command("hlvis", "output.map")
+	vis_err := vis_cmd.Run()
+	check(vis_err)
 }
